2262adac3eb9ccfb0c56cf507969b122d56e1bc5
Patryk Barć <patrykbarc.design@gmail.com>
2025-10-13 21:27:03 +0200

    chore: tailwind and shadcn init

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    conditions: ["source"],
-  },
-});
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		conditions: ["source"],
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+})
