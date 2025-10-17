729d9fbc1f88702cf80b36d6d383f0da6c27cee3
Patryk Barć <patrykbarc.design@gmail.com>
2025-10-17 22:19:26 +0200

    chore(packages): add shared types

---
+++ packages/types/src/types.ts
@@ -0,0 +1,19 @@
+export type ApiResponse = {
+	title: string
+	overall_score: {
+		label: string
+		range: string
+		score: string
+		weighting_justification: {
+			"ATS Compatibility": string
+			"Job Market Alignment": string
+			"Competitive Differentiation": string
+		}
+		justification: string
+	}
+	sections: {
+		strengths: string[]
+		areas_for_improvement: string[]
+		recommendations: string[]
+	}
+}
