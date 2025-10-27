0b0ee02752bf23737dfda448e20f8af75114eec1
Patryk Barć <patrykbarc.design@gmail.com>
2025-10-27 22:45:25 +0100

    feat(web): add health route

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,6 +1,14 @@
 import app from "./app"
 import config from "./config/config"
 
+app.get('/health', (_, res) => {
+  res.status(200).json({
+    status: 'healthy',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime()
+  })
+})
+
 app.listen(config.port, () => {
 	console.log(`Server running on http://localhost:${config.port}`)
 })
