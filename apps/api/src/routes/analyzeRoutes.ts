d88c3054609943486835400ee37cb80f5b23e295
Patryk Barć <patrykbarc.design@gmail.com>
2025-11-15 16:25:09 +0100

    fix(api): move multipartParser only to /analyze route

--- apps/api/src/routes/analyzeRoutes.ts
+++ apps/api/src/routes/analyzeRoutes.ts
@@ -1,11 +1,20 @@
 import { AnalysisParamsSchema, FileSchema } from '@monorepo/schemas'
 import { Router } from 'express'
+import multer from 'multer'
 import { createAnalyze, getAnalysys } from '../controllers/analyseController'
 import { validateData, validateFile } from '../middleware/validateEntries'
 
+const upload = multer({ storage: multer.memoryStorage() })
+const multipartParser = upload.single('file')
+
 const router: Router = Router()
 
-router.post('/analyze', validateFile(FileSchema), createAnalyze)
+router.post(
+  '/analyze',
+  multipartParser,
+  validateFile(FileSchema),
+  createAnalyze
+)
 router.get('/analysis/:id', validateData(AnalysisParamsSchema), getAnalysys)
 
 export default router
--- apps/api/src/routes/routes.ts
+++ apps/api/src/routes/routes.ts
@@ -1,12 +1,6 @@
-import type { Application } from 'express'
-import multer from 'multer'
+import { Application } from 'express'
 import analyzeRoutes from './analyzeRoutes'
 
-const upload = multer({ storage: multer.memoryStorage() })
-
-const multipartParser = upload.single('file')
-
 export const routes = (app: Application) => {
-  app.use(multipartParser)
   app.use('/api/cv', analyzeRoutes)
 }
