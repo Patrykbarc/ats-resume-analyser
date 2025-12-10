e677b0f7dd694dfb193c34c27eea069fb1465b30
Patryk Barć <patrykbarc.design@gmail.com>
2025-12-10 21:22:45 +0100

    fix(web): increase timeout for API client to 120 seconds

--- apps/web/src/api/apiClient.ts
+++ apps/web/src/api/apiClient.ts
@@ -9,7 +9,7 @@ import { StatusCodes } from 'http-status-codes'
 
 export const apiClient = axios.create({
   baseURL: `${getEnvs().VITE_API_URL}/api`,
-  timeout: 60_000,
+  timeout: 120_000,
   withCredentials: true
 })
 
